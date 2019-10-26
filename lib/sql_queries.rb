e97bc817e1892a0388d01a0cff239c2be352605c
Baktiar Miah <baktiar.m@gmail.com>
2019-10-26 16:15:55 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,6 +9,7 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
   "SELECT title.projects, amount.pledges
    FROM projects
+    ON projects.id = pledges.project_id
    GROUP_BY projects.title;
   "
 end
