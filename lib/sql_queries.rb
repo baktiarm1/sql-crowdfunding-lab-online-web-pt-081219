da4a8a573c94d7e559fa7c97b394f70df21dbc45
Baktiar Miah <baktiar.m@gmail.com>
2019-10-26 16:17:14 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -18,7 +18,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
   "SELECT user.name, user.age, SUM(amount.pledges)
    FROM users
       ON users.id =pledges.user_id
-    GROUP_BY users.name;
+    ORDER_BY users.name;
   "
 end
 
